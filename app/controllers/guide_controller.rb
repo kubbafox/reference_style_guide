fbdd97d7eb7a9f675d177fad62ec6af836ef6492
Jim Lindstrom <jim.lindstrom@gmail.com>
2011-08-02 21:31:13 -0400

    Initial commit

---
+++ app/controllers/guide_controller.rb
@@ -0,0 +1,25 @@
+class GuideController < ApplicationController
+  def index
+    # FIXME: this is the least DRY solution possible...
+    case params[:guide] || nil
+      when 'widget'
+        render 'widget'
+      when 'button'
+        render 'button'
+      when 'box'
+        render 'box'
+      when 'link'
+        render 'link'
+      when 'font'
+        render 'font'
+      when 'color'
+        render 'color'
+      when 'homepage'
+        render 'homepage'
+      when 'search'
+        render 'search'
+    end
+
+  end
+
+end
